5a79e577fff8937ecb6909a78fc7dd30c9061a7f
Trop <john-outsider@hotmail.com>
2016-05-05 00:12:30 +0300

    Implemented "IsCreditCardNumberValid" function.

--- CreditCardTestApp/CreditCardTestApp/CreditCardTestApp.cpp
+++ CreditCardTestApp/CreditCardTestApp/CreditCardTestApp.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include <string>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
@@ -20,9 +21,9 @@ enum class CreditCardVendor
 	JCB
 };
 
-CreditCardVendor GetCreditCardVendor(const string &inCreditCardNumber)
+CreditCardVendor GetCreditCardVendor(const string &creditCardNumber)
 {
-	auto number = inCreditCardNumber;
+	string number = creditCardNumber;
 	number.erase(remove_if(number.begin(), number.end(), isspace), number.end());
 	int length = number.length();
 	int firstPairOfDigits = stoi(number.substr(0,2));
@@ -64,10 +65,36 @@ CreditCardVendor GetCreditCardVendor(const string &inCreditCardNumber)
 	}
 }
 
+bool IsCreditCardNumberValid(const string &creditCardNumber)
+{
+	bool isOdd = true;
+	int digit = 0;
+	int sum = 0;
+	string number = creditCardNumber;
+	number.erase(remove_if(number.begin(), number.end(), isspace), number.end());
+	reverse(number.begin(), number.end());
+	int length = number.length();
+
+	if (length < MIN_LENGTH || length > MAX_LENGTH || !all_of(number.begin(), number.end(), isdigit))
+	{
+		return false;
+	}
+	for (char& c : number) 
+	{
+		digit = c - '0';
+		sum += isOdd ? digit : digit / 5 + (2 * digit) % 10;
+		isOdd = !isOdd;
+	}
+	return sum % 10 == 0;
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	GetCreditCardVendor("4390 4566 4213 5673");
+	cout << (int)GetCreditCardVendor("4390 4566 4213 5673") << endl;
+	cout << IsCreditCardNumberValid("4390 4566 4213 5673") << endl;
+	cout << IsCreditCardNumberValid("1234 5678 1234 5670") << endl;
+	
 	return 0;
 }
 
